4844344c9b89dd1743507b05ab6557b2646a8e41
Washington Xavier Yépez Yépez <wyepez@outlook.com>
2021-02-15 11:20:03 -0500

    How to execute C# code at runtime

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace DispositionRule
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public int SubGroupId { get; set; }
+        public int ArticleMaterialId { get; set; }
+    }
+
+    public class Stock
+    {
+        public int Id { get; set; }
+        public int Weight { get; set; }
+        public int Thickness { get; set; }
+        public int Width { get; set; }
+        public int Diameter { get; set; }
+        public Article Article { get; set; }
+    }
+
+    public class GlobalParameters
+    {
+        public Stock StockBelow { get; set; }
+        public Stock StockBelowSupport { get; set; }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var stockBelow = new Stock
+            {
+                Id = 1,
+                Weight = 100,
+                Thickness = 2,
+                Width = 500,
+                Diameter = 1000,
+                Article = new Article
+                {
+                    Id = 1,
+                    GroupId = 1,
+                    SubGroupId = 1,
+                    ArticleMaterialId = 1,
+                },
+            };
+            var stockBelowSupport = new Stock
+            {
+                Id = 2,
+                Weight = 120,
+                Thickness = 1,
+                Width = 600,
+                Diameter = 900,
+                Article = new Article
+                {
+                    Id = 1,
+                    GroupId = 1,
+                    SubGroupId = 2,
+                    ArticleMaterialId = 1,
+                },
+            };
+
+            var scoreRules = new[] {
+                "if (StockBelow.Article.SubGroupId == 1 && StockBelowSupport.Article.SubGroupId == 1) return 5; else if (StockBelow.Article.SubGroupId == 2 || StockBelowSupport.Article.SubGroupId == 2) return 3; else return 0;"
+            };
+
+            var filterRules = new[] {
+                "StockBelow is not null && StockBelowSupport is not null",
+            };
+
+            // Create an empty state
+            var state = await CSharpScript.RunAsync("", globals: new GlobalParameters { StockBelow = stockBelow, StockBelowSupport = stockBelowSupport });
+
+            // Evaluate each score rule
+            foreach (var rule in scoreRules)
+            {
+                try
+                {
+                    state = await state.ContinueWithAsync(rule);
+                    Console.WriteLine(String.Format("Rule '{0}' was evaluated as {1}", rule, (int)state.ReturnValue));
+                }
+                catch (CompilationErrorException exception)
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, exception.Diagnostics));
+                }
+            }
+
+            // Evaluate each filter rule
+            foreach (var rule in filterRules)
+            {
+                try
+                {
+                    state = await state.ContinueWithAsync(rule);
+                    Console.WriteLine(String.Format("Rule '{0}' was evaluated as {1}", rule, (bool)state.ReturnValue));
+                }
+                catch (CompilationErrorException exception)
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, exception.Diagnostics));
+                }
+            }
+        }
+    }
+}
